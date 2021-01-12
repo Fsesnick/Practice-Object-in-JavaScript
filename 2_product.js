d3fe1fea2909a0a8b73a04837ae5234c7d420a1d
Fsesnick <fernandasesnick@gmail.com>
2021-01-12 18:18:40 +0000

    Solution 2_products.js

--- 2_product.js
+++ 2_product.js
@@ -14,27 +14,49 @@ If your program is stuck in an infinite loop, you can break out of the program b
 // -- unit_price
 // before moving to the next step, make sure you've created the object correctly by logging it to the console like this: console.log(product)
 
+
+let produto = {
+  nome: "Cadeira",
+  estoque: 20,
+  preco_unitario: 45.99
+};
+
 // 2. Create a function named addInventory(). The function should accept 2 parameters -- the product object, and the number to add to the inventory. The function adjusts the product object's inventory property by adding the number passed into the function. 
 // For example, if the inventory of the product is currently 2, calling addInventory(product, 3) will set the value of inventory to 5
 /* Add a console.log() message to the function that includes a message that looks something like this:
+
 "3 chairs added to the inventory"
 In this example, "chair" is the name of the product.
 */
 
+function addEstoque(prod, quantidade){
+
+  prod.estoque += quantidade;
+  console.log(quantidade + " " + prod.nome + "(s) adicionada (s) ao estoque.");
+  console.log(prod.estoque + " " +prod.nome + "(s) em estoque");
+}
+
 // 3. Call the addInventory() function
 
-
+addEstoque(produto,3);
 // 4. Create a function named processSale(). The function should accept 2 parameters -- the product object, and the number to of products that were sold. The function adjusts the product object's inventory property by subtracting the number passed into the function. The function also returns the total sale which is the unit_price multiplied by the number passed into the function
 // For example, if the inventory of the product is currently 5 and the unit_price is 10, calling processSale(product, 2) will set the value of inventory to 3 and return the value of 20. 
 /* Add a console.log() message to the function that includes a message that looks something like this:
 "2 chairs sold"
 In this example, "chair" is the name of the product.
 */
+function processarVenda(prod, quantidade){
+  prod.estoque -= quantidade;
+  console.log(quantidade + " " +prod.nome + " vendidas");
+  return quantidade* prod.preco_unitario;
 
+}
 
 /* 5. Call the processSale() function. When you call the processSale() function log the return value to the console so the message looks something like this:
 "Total sale: $20"
 */
+console.log("Vendas totais: R$" + processarVenda(produto,3));
 
+//processarVenda(produto,5);
 
 // 6. Run your code by typing node 2_product.js in the console below
