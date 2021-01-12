636e5b71ad1fdcc7c8ad8509daa56f885e6749d9
Fsesnick <fernandasesnick@gmail.com>
2021-01-12 18:45:48 +0000

    +-final_products.js final

--- 3_final_products.js
+++ 3_final_products.js
@@ -14,17 +14,53 @@ If your program is stuck in an infinite loop, you can break out of the program b
 // -- inventory
 // -- unit_price
 
-
+let produto = [
+  {
+  nome: "Cadeira",
+  estoque: 5,
+  preco_unitario: 45.99
+  },
+   {
+  nome: "mesa",
+  estoque: 10,
+  preco_unitario: 123.75
+  },
+   {
+  nome: "sofá",
+  estoque: 2,
+  preco_unitario: 399.50
+   
+   }
+];
 // 3. Create a function named listProducts(). The function should accept 1 parameter -- the array of products. It should return an array of just the names of the products.
+
+function  listarProdutos(prods){
+  let produto_nomes = [];
+  for (let i = 0; i < prods.length; i +=1){
+     produto_nomes.push(prods[i].nome);
+  
+  }
+  return produto_nomes;
+}
 
 
 // 4. Call the listProducts() function and log the returned value to the console.
 
-
+console.log( listarProdutos (produto));
 // 5. Create a function names totalValue(). The function should accept 1 parameter -- the array of products. It should return the total value of all of the products in the array. You calculate the value of one product by multiplying the inventory value by the unit_price value
 
+function valorTotal (prods) {
+  let valor_estoque = 0.5;
+  
+  for(let i = 0; i< prods.length; i += 1){
+    valor_estoque += prods[i].estoque * prods[i].preco_unitario;
+  
+  }
+  return valor_estoque;
+} 
 
 // 6. Call the totalValue() function and log the returned value to the console.
 
+console.log(valorTotal(produto));
 
 // 7. Run your code by typing node 3_final_products.js in the console below
